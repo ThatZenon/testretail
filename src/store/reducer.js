719d8e24092c5285ba5d9a261fa720c31960b477
YashChaudhari241 <55353317+YashChaudhari241@users.noreply.github.com>
2021-06-06 20:03:31 +0530

    Updated reducer.js to support firebase auth

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,47 +1,55 @@
 
 
 const initialState = {
-    login:false,
-    usercart:[],
-    darkMode:false
+    login: false,
+    usercart: [],
+    authToken: null,
+    darkMode: false
 }
-let tmp=null
-const reducer = (state = initialState,action) =>{
-    switch(action.type)
-    {
+let tmp = null
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case 'TOGGLE_LOGIN':
+            let token = (action.payload.value) ? action.payload.token : null;
             return {
                 ...state,
-                login: action.payload.value
+                login: action.payload.value,
+                authToken: token,
+                usercart: []
             }
         case 'TOGGLE_DARK':
-            return{
+            return {
                 ...state,
                 darkMode: !state.darkMode
             }
         case 'ADD_ITEM':
-            return{
+            return {
                 ...state,
-                usercart:[...state.usercart,action.payload.itm]
+                usercart: [...state.usercart, action.payload.itm]
             }
         case 'RMV_ITEM':
             tmp = [...state.usercart]
-            tmp.splice(action.payload.id,1)
-            return{
+            tmp.splice(action.payload.id, 1)
+            return {
                 ...state,
-                usercart:tmp
+                usercart: tmp
+            }
+        case 'EMPTY_CART':
+            return {
+                ...state,
+                usercart: []
             }
         case 'EDIT_ITEM':
             tmp = [...state.usercart]
-            tmp.splice(action.payload.id,1)
-            return{
+            tmp.splice(action.payload.id, 1)
+            return {
                 ...state,
-                usercart:[...tmp,action.payload.itm]
-            }    
-        default :
-        return{
-            ...state
-        }
+                usercart: [...tmp, action.payload.itm]
+            }
+        default:
+            return {
+                ...state
+            }
     }
 }
 
